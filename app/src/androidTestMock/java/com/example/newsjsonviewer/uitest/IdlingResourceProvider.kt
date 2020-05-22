86118293cbc51047fad1acd64a563c05b7afdd03
Pedro Roig <pedro.roiggimenez@soprasteria.com>
2020-05-22 16:21:24 +0200

    fix incompatibility between CountingIdlingResource and unit tests

--- app/src/androidTestMock/java/com/example/newsjsonviewer/uitest/IdlingResourceProvider.kt
+++ app/src/androidTestMock/java/com/example/newsjsonviewer/uitest/IdlingResourceProvider.kt
@@ -7,8 +7,17 @@ import androidx.test.espresso.idling.CountingIdlingResource
 /**
  * Provides [CountingIdlingResource] for Espresso testing. This class  has a double in production
  * code that returns a null value
+ *
+ * The value for UI test is wrapped in a try / catch: this is because the unit tests will pick
+ * this file instead of the production one, then [CountingIdlingResource] initialization would
+ * fail as it uses the Android Framework.
+ * In this case we return null, as IdlingResources are not used in unit tests
  */
 object IdlingResourceProvider {
     fun provideIdlingResource(className: String): CountingIdlingResource? =
+        try {
             CountingIdlingResource(className)
+        } catch (e: Exception) {
+            null
+        }
 }
